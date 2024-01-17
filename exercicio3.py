45374b9665f82f331584e43e94a6e3e8717400a6
PuriNakayama <priscila.nakayama@yahoo.com>
2024-01-17 15:05:57 -0300

    exercicio3

---
+++ exercicio3.py
@@ -0,0 +1,13 @@
+def calcula_unidades(distancia): #funcao calcula a distancia
+    distancia_m = distancia * 1000
+    distancia_cm = distancia_m * 100
+    distancia_mm = distancia_cm * 10
+    return distancia_m, distancia_cm, distancia_mm
+
+distancia_km = float(input('Informe a quantidade de quilômetros: '))
+
+distancia_m, distancia_cm, distancia_mm = calcula_unidades(distancia_km)
+
+print( f'A distância em metros é: {distancia_m}')
+print( f'A distância em centímetros é: {distancia_cm}' )
+print( f'A distância em milímetros é: {distancia_mm}' )
