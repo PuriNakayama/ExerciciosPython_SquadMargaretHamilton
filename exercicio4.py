e7bb07e8261206345c4a7f654c9860955b515155
PuriNakayama <priscila.nakayama@yahoo.com>
2024-01-17 19:18:51 -0300

    exercicio

--- exercicio4.py
+++ exercicio4.py
@@ -1,13 +1,22 @@
-#funcao calcula o consumo medio de gasolina
-def calc_consumo_medio(distancia_km, combustivel_consumido):
-    consumo_medio =  distancia_km / combustivel_consumido    
-    return consumo_medio
+# usuario informa a distância da viagem
+distancia = float(input("Digite a distância da viagem em km: "))
 
-#informa o km e a quantidade de litros de combustivel
-distancia_km = float(input('Digite a quantidade de quilômetros: '))
-combustivel_consumido = float(input('Informe a quantidade de litros de combustível consumido: '))
+# calculo do tempo de viagem em cada meio de transporte
+tempo_aviao = distancia / 600
+tempo_carro = distancia / 100
+tempo_onibus = distancia / 80
 
-consumo_medio = calc_consumo_medio(distancia_km, combustivel_consumido)
+# funcao para converter tempo de horas e minutos
+def converter_tempo(tempo):
+    horas = int(tempo)
+    minutos = int((tempo - horas) * 60)
+    return horas, minutos
 
-#retorna o consumo medio
-print(f'O consumo médio em km/l é: {consumo_medio} litros')
+tempo_aviao_conv = converter_tempo(tempo_aviao)
+tempo_carro_conv = converter_tempo(tempo_carro)
+tempo_onibus_conv = converter_tempo(tempo_onibus)
+
+# Imprimindo os resultados
+print("Tempo de viagem por avião: ", tempo_aviao_h_m[0], "horas e", tempo_aviao_h_m[1], "minutos")
+print("Tempo de viagem no carro: ", tempo_carro_h_m[0], "horas e", tempo_carro_h_m[1], "minutos")
+print("Tempo de viagem no ônibus: ", tempo_onibus_h_m[0], "horas e", tempo_onibus_h_m[1], "minutos")
