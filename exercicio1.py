ad039c1f554cc089888831f4f0ea99ebfbdcd9e7
PuriNakayama <priscila.nakayama@yahoo.com>
2024-01-17 12:40:23 -0300

    exercicio1-modificacao

--- exercicio1.py
+++ exercicio1.py
@@ -6,22 +6,21 @@ def menu(): # vai ser chamado na condicao while
     print('4 - Divisao')
     
     
-def soma(num1,num2,num3): # funcao soma
-    return num1+num2+num3
+def soma(num1,num2): # funcao soma
+    return num1+num2
     
-def subtracao(num1,num2,num3): # funcao subtracao
-    return num1-num2-num3
+def subtracao(num1,num2): # funcao subtracao
+    return num1-num2
 
-def multiplicacao(num1,num2,num3): #funcao multiplicacao
-    return num1*num2*num3
+def multiplicacao(num1,num2): #funcao multiplicacao
+    return num1*num2
 
-def divisao(num1,num2,num3): #funcao divisao
-    return num1/num2/num3
+def divisao(num1,num2): #funcao divisao
+    return num1/num2
     
-print("Digite três números") #usuario insere os valores 
+ #usuario insere os valores 
 number1 = float(input('Digite o primeiro numero: '))
 number2 = float(input('Digite o segundo numero: '))
-number3 = float(input('Digite o terceiro numero: '))
 
 
 while True: #condicao, enquanto o usuario digitar um numero, o codigo roda
@@ -30,15 +29,15 @@ while True: #condicao, enquanto o usuario digitar um numero, o codigo roda
     if opcao == 0: #se o usuario digitar 0, encerra o codigo
         break
     elif opcao == 1: #senao continua rodando
-        print('Resultado da soma:', soma(number1, number2, number3)) #resultado da soma
+        print('Resultado da soma:', soma(number1, number2)) #resultado da soma
     elif opcao == 2:
-        print('Resultado da subtração:', subtracao(number1, number2, number3)) #resultado da subtracao
+        print('Resultado da subtração:', subtracao(number1, number2)) #resultado da subtracao
     elif opcao == 3:
-        print('Resultado da multiplicação:', multiplicacao(number1, number2, number3)) # resultado da multiplicacao
+        print('Resultado da multiplicação:', multiplicacao(number1, number2)) # resultado da multiplicacao
     elif opcao == 4:
-        if number2 == 0 or number3 == 0: # #verificacao se o usuario digita 0 
+        if number2 == 0: # #verificacao se o usuario digita 0 
             print('Não é possível realizar a divisão por zero.') 
         else:
-            print('Resultado da divisão:', divisao(number1, number2, number3)) #resultado da divisao
+            print('Resultado da divisão:', divisao(number1, number2)) #resultado da divisao
     else:
         print('Opção inválida. Tente novamente.') #Mensagem de opcao invalida
