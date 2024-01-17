62ddf80b1921fd6769e9b489cdcc1791582e1d76
PuriNakayama <priscila.nakayama@yahoo.com>
2024-01-17 11:58:03 -0300

    modificacao condicaoex1

--- exercicio1.py
+++ exercicio1.py
@@ -26,19 +26,19 @@ number3 = float(input('Digite o terceiro numero: '))
 
 while True: #condicao, enquanto o usuario digitar um numero, o codigo roda
     menu()
-    opcao = int(input('Escolha uma opção de 1 a 4 (ou 0 para sair): ')) #condicao para sair do codigo
+    opcao = int(input('Escolha apenas uma opção de 1 a 4 (ou 0 para sair): ')) #condicao para sair do codigo
     if opcao == 0: #se o usuario digitar 0, encerra o codigo
         break
     elif opcao == 1: #senao continua rodando
-        print("Resultado da soma:", soma(number1, number2, number3)) #resultado da soma
+        print('Resultado da soma:', soma(number1, number2, number3)) #resultado da soma
     elif opcao == 2:
-        print("Resultado da subtração:", subtracao(number1, number2, number3)) #resultado da subtracao
+        print('Resultado da subtração:', subtracao(number1, number2, number3)) #resultado da subtracao
     elif opcao == 3:
-        print("Resultado da multiplicação:", multiplicacao(number1, number2, number3)) # resultado da multiplicacao
+        print('Resultado da multiplicação:', multiplicacao(number1, number2, number3)) # resultado da multiplicacao
     elif opcao == 4:
         if number2 == 0 or number3 == 0: # #verificacao se o usuario digita 0 
-            print("Não é possível realizar a divisão por zero.") 
+            print('Não é possível realizar a divisão por zero.') 
         else:
-            print("Resultado da divisão:", divisao(number1, number2, number3)) #resultado da divisao
+            print('Resultado da divisão:', divisao(number1, number2, number3)) #resultado da divisao
     else:
-        print("Opção inválida. Tente novamente.") #Mensagem de opcao invalida
+        print('Opção inválida. Tente novamente.') #Mensagem de opcao invalida
