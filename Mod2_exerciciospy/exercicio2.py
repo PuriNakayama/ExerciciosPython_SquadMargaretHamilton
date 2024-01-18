c02fdb5faf7e704d4b1661556eb14199a94f9f3c
PuriNakayama <priscila.nakayama@yahoo.com>
2024-01-17 21:27:32 -0300

    exercicios2

---
+++ Mod2_exerciciospy/exercicio2.py
@@ -0,0 +1,12 @@
+# usuário informa em qual turno ele estuda
+turno = input('Qual turno você estuda? Digite M-matutino, V-vespertino ou N-noturno: ')
+
+# condicao para determinar qual mensagem vai ser exibida
+if turno.lower() == "m":
+    print("Bom Dia!")
+elif turno.lower() == "v":
+    print("Boa Tarde!")
+elif turno.lower() == "n":
+    print("Boa Noite!")
+else:
+    print("Valor Inválido!")
